092d99fd9ad14fc1b1c41e684b8299e1d690a768
amitw-19 <tarunwarathe@gmail.com>
2023-03-29 20:05:28 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
-import React, {useEffect, useState} from 'react'
-import '../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
-//code here 
+  //code here
+  const [isActive, setActive] = useState("false");
+
+  useEffect(() => {
+    setActive(!isActive);
+  }, []);
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className={isActive ? "blueColor" : "redColor"}>Newton School</p>
+      <button id="button" onClick={() => setActive(!isActive)}>
+        Change Style
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
